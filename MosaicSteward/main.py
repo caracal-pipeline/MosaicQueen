3dbabff8e54e9ce080a4fbfc7fa197f8fda316f5
Sarah White <sarahwhite.astro@gmail.com>
2019-12-09 14:18:32 +0200

    More files with name change

---
+++ MosaicSteward/main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+# ------------------------------------------------------------------------------------------------------
+# Author of package: Sarah White (sarahwhite.astro@gmail.com) and Sphe Makhathini (sphemakh@gmail.com)
+# Based on a mosaicking script by Paolo Serra (paolo80serra@gmail.com)
+# ------------------------------------------------------------------------------------------------------
+
+from MosaicSteward import make_mosaic
+from argparse import ArgumentParser
+import MosaicSteward
+import os
+import sys
+
+log = mosaic_steward.log
+
+# So that error handling is compatible with Python 2 as well as Python 3
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+def main(argv):
+
+    parser = ArgumentParser(description="Run make_mosaic over the targets")
+
+    parser.add_argument("-i", "--input",
+                        help="The directory that contains the (2D or 3D) images and beams.")
+    parser.add_argument("-m", "--mosaic-type",
+                        help="State 'continuum' or 'spectral' as the type of mosaic to be made.")
+    parser.add_argument("-d", "--domontage", action="store_true",
+                        help="Use montage for regridding the images and beams.")
+    parser.add_argument("-c", "--cutoff", type=float, default=0.1,
+                         help="The cutoff in the primary beam to use (assuming a Gaussian at the moment)."
+                              "E.g. The default of 0.1 means going down to the 10 percent level for each pointing.")
+    parser.add_argument("-n", "--name", default="mymosaic",
+                        help="The prefix to be used for output files.")
+    parser.add_argument("-t", "--target-images", action="append",
+                         help="The filenames of each target/pointing image to be mosaicked. A suffix of 'image.fits' is expected, and this is replaced by 'pb.fits' in order to locate the corresponding beams (which are also required as input).")
+    parser.add_argument("-o", "--output",
+                         help="The directory for all output files.")
+
+    args = parser.parse_args(argv)
+    input_dir = args.input
+    mosaic_type = args.mosaic_type
+    cutoff = args.cutoff
+    outname = args.name
+    output_dir = args.output
+
+    if args.target_images: 
+        log.info('Target images = {}'.format(" ".join(args.target_images)))
+        images = args.target_images
+    else:
+        log.error(
+            "Must specify the (2D or 3D) images to be mosaicked, each prefixed by '-t '.")
+        raise LookupError("Must specify the (2D or 3D) images to be mosaicked, each prefixed by '-t '.")
+
+    # Throw an error if the user provides only one image
+    if len(images) < 2:
+        log.error('At least two images must be specified for mosaicking')
+        raise ValueError('At least two images must be specified for mosaicking')
+
+    beams = [tt.replace('image.fits', 'pb.fits') for tt in images]
+    imagesR = [tt.replace('image.fits', 'imageR.fits') for tt in images]
+    beamsR = [tt.replace('image.fits', 'pbR.fits') for tt in images]
+
+    for tt in images:
+        try:
+            open(input_dir+'/'+tt)
+        except FileNotFoundError:
+            log.error('File {0:s} does not exist'.format(input_dir+'/'+tt))
+            raise FileNotFoundError('File {0:s} does not exist'.format(input_dir+'/'+tt))
+
+    for bb in beams:
+        try:
+            open(input_dir+'/'+bb)
+        except FileNotFoundError:
+            log.error('File {0:s} does not exist'.format(input_dir+'/'+bb)) 
+            raise FileNotFoundError('File {0:s} does not exist'.format(input_dir+'/'+bb))
+
+    log.info('All images and beams found on disc')
+
+
+    if args.domontage:
+        make_mosaic.use_montage_for_regridding(
+            input_dir, output_dir, mosaic_type, images, beams, imagesR, beamsR, outname)
+    else:
+        log.info(
+            'Will use mosaic header {0:s}.hdr and regridded images and beams available on disc'.format(outname))
+
+    make_mosaic.check_for_regridded_files(output_dir, imagesR, beamsR)
+
+    make_mosaic.make_mosaic_using_beam_info(input_dir, output_dir, mosaic_type, outname, imagesR, beamsR, cutoff, images)
+
+    # Move the log file to the output directory
+    os.system('mv log-make_mosaic.txt '+output_dir+'/')
+
+    return 0
---
+++ MosaicSteward/make_mosaic.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+# ------------------------------------------------------------------------------------------------------
+# Author of package: Sarah White (sarahwhite.astro@gmail.com) and Sphe Makhathini (sphemakh@gmail.com)
+# Based on a mosaicking script by Paolo Serra (paolo80serra@gmail.com)
+# ------------------------------------------------------------------------------------------------------
+
+from astropy.io import fits
+import subprocess
+from sys import argv
+import sys
+import os
+import numpy as np
+import MosaicSteward
+import argparse
+
+log = mosaic_steward.log
+
+# So that error handling is compatible with Python 2 as well as Python 3
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+# -------------------- Edited functions from the original script ------------------------ #
+
+def create_montage_list(inputfiles, outputfile):
+    list_file = open(outputfile, 'w')
+    # Need to know length of the longest filename so that none of them get truncated
+    max_filename_length = len( max(inputfiles, key=len) )
+    start_of_line = '|'
+    start_and_whitespace = start_of_line.ljust(max_filename_length) # This will add trailing whitespace of the required length
+    list_file.write( start_and_whitespace+'fname|\n' )
+    list_file.write( start_and_whitespace+' char|\n' )
+    for ff in inputfiles:
+        list_file.write(' {0:s}\n'.format(ff))
+    list_file.close()
+
+
+def Run(command, verb1=1, verb2=0, getout=0):
+    if verb1:
+        log.info('      '+command)
+    result = subprocess.check_output(command.split())
+    if verb2:
+        for jj in result:
+            log.info(jj)
+    if getout:
+        return result
+
+
+def make_mosaic_header(mosaic_type, t_head):
+    astro_t_head = fits.Header()
+    for hh in t_head.keys():
+        astro_t_head[hh] = t_head[hh]
+    if mosaic_type == 'spectral':
+        for hh in 'crpix1,crval1,cdelt1,crpix2,crval2,cdelt2,crpix3,crval3,cdelt3'.split(','):
+            astro_t_head[hh] = float(astro_t_head[hh])
+        for hh in 'naxis,naxis1,naxis2,naxis3,equinox'.split(','):
+            astro_t_head[hh] = int(astro_t_head[hh])
+    if mosaic_type == 'continuum':
+        for hh in 'crpix1,crval1,cdelt1,crpix2,crval2,cdelt2'.split(','):
+            astro_t_head[hh] = float(astro_t_head[hh])
+        for hh in 'naxis,naxis1,naxis2,equinox'.split(','):
+            astro_t_head[hh] = int(astro_t_head[hh])
+    for hh in 'ctype1,ctype2'.split(','):
+        astro_t_head[hh] = astro_t_head[hh].replace("'", "")
+    return astro_t_head
+
+
+# ---------------------------- New/re-structured functions --------------------------------- #
+
+def use_montage_for_regridding(input_dir, output_dir, mosaic_type, images, beams, imagesR, beamsR, outname):
+    log.info('Running montage tasks to create mosaic header ...')
+    # Create an image list
+    create_montage_list(images, '{0:s}/{1:s}_fields'.format(output_dir,outname))
+    #print(sys.stdout)
+    Run('mImgtbl -t {0:s}/{1:s}_fields {2:s} {0:s}/{1:s}_fields.tbl'.format(output_dir,outname,input_dir))
+    # Create mosaic header
+    Run('mMakeHdr {0:s}/{1:s}_fields.tbl {0:s}/{1:s}.hdr'.format(output_dir,outname))
+    
+    # Which montage program is used for regridding depends on whether the image is 2D or 3D
+    if mosaic_type == 'spectral':
+        montage_projection = 'mProjectCube'
+        montage_add = 'mAddCube'
+    if mosaic_type == 'continuum':
+        montage_projection = 'mProject'
+        montage_add = 'mAdd'
+
+    log.info('Running montage tasks to regrid the input images ...')
+    # Reproject the input images
+    for cc in images:
+        Run(montage_projection + ' {0:s}/{1:s} {2:s}/{3:s} {2:s}/{4:s}.hdr'.format(
+            input_dir, cc, output_dir, cc.replace('image.fits', 'imageR.fits'), outname))
+    # Create a reprojected-image metadata file
+    create_montage_list(imagesR, '{0:s}/{1:s}_fields_regrid'.format(output_dir,outname))
+    Run('mImgtbl -d -t {0:s}/{1:s}_fields_regrid {0:s} {0:s}/{1:s}_fields_regrid.tbl'.format(output_dir,outname)) # '-d' flag added to aid de-bugging
+    # Co-add the reprojected images
+    #Run(montage_add + ' -p . {0:s}_fields_regrid.tbl {0:s}.hdr {0:s}.fits'.format(outname))
+    
+    log.info('Running montage tasks to regrid the input beams ...')
+    # Reproject the input beams
+    for bb in beams:
+        Run(montage_projection + ' {0:s}/{1:s} {2:s}/{3:s} {2:s}/{4:s}.hdr'.format(
+            input_dir, bb, output_dir, bb.replace('pb.fits', 'pbR.fits'), outname))
+    # Create a reprojected-beams metadata file
+    create_montage_list(beamsR, '{0:s}/{1:s}_beams_regrid'.format(output_dir,outname))
+    Run('mImgtbl -t {0:s}/{1:s}_beams_regrid {0:s} {0:s}/{1:s}_beams_regrid.tbl'.format(output_dir,outname))
+    # Co-add the reprojected beams
+    #Run(montage_add + ' -p . {0:s}_beams_regrid.tbl {0:s}.hdr {0:s}pb.fits'.format(outname))
+
+    return 0
+
+
+def check_for_regridded_files(output_dir, imagesR, beamsR):
+    # As the regridded files were produced by montage_mosaic, we expect them to be in the output directory
+
+    for cc in imagesR:
+        try:
+            open(output_dir+'/'+cc)
+        except FileNotFoundError:
+            log.error('File {0:s} does not exist'.format(output_dir+'/'+cc))
+            raise FileNotFoundError('File {0:s} does not exist'.format(output_dir+'/'+cc))
+
+    for bb in beamsR:
+        try:
+            open(output_dir+'/'+bb)
+        except FileNotFoundError:
+            log.error('File {0:s} does not exist'.format(output_dir+'/'+bb))
+            raise FileNotFoundError('File {0:s} does not exist'.format(output_dir+'/'+bb))
+
+    log.info('All regridded images and beams found on disc')
+
+    return 0
+
+
+def make_mosaic_using_beam_info(input_dir, output_dir, mosaic_type, outname, imagesR, beamsR, cutoff, images):
+
+    log.info('Mosaicking ...')
+    moshead = [jj.strip().replace(' ', '').split('=')
+            for jj in open('{0:s}/{1:s}.hdr'.format(output_dir,outname)).readlines()]
+    if ['END'] in moshead:
+        del(moshead[moshead.index(['END'])])
+    moshead = {k: v for (k, v) in moshead}   # Creating a dictionary, where 'k' stands for 'keyword' and 'v' stands for 'value'
+    if mosaic_type == 'spectral':
+        mos_array = np.zeros((int(moshead['NAXIS3']), int(
+            moshead['NAXIS2']), int(moshead['NAXIS1'])), dtype='float32')
+        norm_array = np.zeros((int(moshead['NAXIS3']), int(
+            moshead['NAXIS2']), int(moshead['NAXIS1'])), dtype='float32')
+    if mosaic_type == 'continuum':
+        mos_array = np.zeros((int(moshead['NAXIS2']), int(moshead['NAXIS1'])), dtype='float32')
+        norm_array = np.zeros((int(moshead['NAXIS2']), int(moshead['NAXIS1'])), dtype='float32')
+    for ii, bb in zip(imagesR, beamsR):
+        log.info('Adding {0:s} to the mosaic ...'.format(ii))
+        f = fits.open(output_dir+'/'+ii)  # i.e. open a specific re-gridded image
+        head = f[0].header
+        g = fits.open(output_dir+'/'+bb)  # i.e. open a specific re-gridded beam
+        y1 = int(float(moshead['CRPIX2']) - head['CRPIX2'])
+        y2 = int(float(moshead['CRPIX2']) - head['CRPIX2'] + head['NAXIS2'])
+        x1 = int(float(moshead['CRPIX1']) - head['CRPIX1'])
+        x2 = int(float(moshead['CRPIX1']) - head['CRPIX1'] + head['NAXIS1'])
+        # mosaicking with no PB correction
+        # norm_array[:,y1:y2,x1:x2]+=~np.isnan(f[0].data)
+        # mos_array[:,y1:y2,x1:x2]+=np.nan_to_num(f[0].data)
+        # mosaicking with PB correction
+        if mosaic_type == 'spectral':
+            norm_array[:, y1:y2, x1:x2] += (np.nan_to_num(g[0].data)
+                                      * (np.nan_to_num(g[0].data) > cutoff))**2
+            mos_array[:, y1:y2, x1:x2] += np.nan_to_num(f[0].data)*np.nan_to_num(
+                g[0].data)*(np.nan_to_num(g[0].data) > cutoff)
+        if mosaic_type == 'continuum':
+            norm_array[y1:y2, x1:x2] += (np.nan_to_num(g[0].data)
+                                      * (np.nan_to_num(g[0].data) > cutoff))**2
+            mos_array[y1:y2, x1:x2] += np.nan_to_num(f[0].data)*np.nan_to_num(
+                g[0].data)*(np.nan_to_num(g[0].data) > cutoff)
+        f.close()
+        g.close()
+
+    norm_array[norm_array == 0] = np.nan
+    moshead = make_mosaic_header(mosaic_type, moshead)
+    if mosaic_type == 'spectral':
+        f = fits.open(input_dir+'/'+images[0]) 
+        zhead = f[0].header
+        moshead['ctype3'] = zhead['ctype3']
+    fits.writeto('{0:s}/{1:s}.fits'.format(output_dir,outname), mos_array /
+                 norm_array, overwrite=True, header=moshead)
+    fits.writeto('{0:s}/{1:s}_noise.fits'.format(output_dir,outname), 1. /
+                 np.sqrt(norm_array), overwrite=True, header=moshead)
+    fits.writeto('{0:s}/{1:s}_weights.fits'.format(output_dir,outname),
+                 np.sqrt(norm_array), overwrite=True, header=moshead)
+
+    log.info('The following mosaic FITS were written to disc: {0:s}.fits {0:s}_noise.fits {0:s}_weights.fits'.format(outname))
+    log.info('Mosaicking completed')
+
+    return 
