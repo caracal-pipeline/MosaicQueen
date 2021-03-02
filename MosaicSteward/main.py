dd5d4e541d805b5d49eb1be8c30dd780b311f48e
Sarah White <sarahwhite.astro@gmail.com>
2021-03-02 15:35:31 +0200

    removed exit() to proceed with next tests

--- MosaicSteward/main.py
+++ MosaicSteward/main.py
@@ -121,8 +121,6 @@ def main(argv):
         log.info(
             'Will use mosaic header {0:s}.hdr and regridded images and beams available on disk'.format(outname))
 
-    exit() ### CHECK REGRIDDING IS STILL WORKING BEFORE PROCEEDING 
-    
     make_mosaic.final_check_for_files(output_dir, imagesR, beamsR)  # This function raises an error and exits if files are not found 
 
     make_mosaic.make_mosaic_using_beam_info(input_dir, output_dir, mosaic_type, 'image', outname, imagesR, beamsR, cutoff, images)
