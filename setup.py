70b8b56d0aa915117e77a5b47f68385571395f9b
Sphesihle Makhathini <sphemakh@gmail.com>
2022-06-16 16:12:55 +0200

    add MANIFEST and long description type

--- setup.py
+++ setup.py
@@ -11,26 +11,26 @@ requirements = [
 "scipy"
 ]
 
+build_root = os.path.dirname(__file__)
+
+def readme():
+    """Get readme content for package long description"""
+    with open(os.path.join(build_root, 'README.rst')) as f:
+        return f.read()
+
 PACKAGE_NAME = 'mosaic-queen'
 __version__ = '1.1.0'
 
 setup(name = PACKAGE_NAME,
     version = __version__,
     description = "A package with mosaicking commands from montage",
+    long_description = readme(),
+    long_description_content_type="text/rst",
     author = "Sarah White and CaraCal pipeline tean",
     author_email = "sarahwhite.astro@gmail.com",
     url = "https://github.com/caracal-pipeline/MosaicQueen",
     packages = find_packages(),
     install_requires = requirements,
     include_package_data = True,
-    license="GNU GPL v2",
     scripts = ["bin/" + i for i in os.listdir("bin")],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Topic :: Scientific/Engineering :: Astronomy"
-    ]
-     )
+    classifiers = [])
